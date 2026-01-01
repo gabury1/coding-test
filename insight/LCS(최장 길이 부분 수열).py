8037b883425dcd20ad5d624282bd4a4db1617b91
@LYST <gabury1@gmail.com>
2026-01-01 13:13:21 +0900

    Create LCS(최장 길이 부분 수열).py

---
+++ insight/LCS(최장 길이 부분 수열).py
@@ -0,0 +1,29 @@
+str1 = input()
+str2 = input()
+
+# 2차원 DP 테이블
+dp = [[0]*(len(str2)+1) for _ in range(len(str1)+1)]
+
+# 테이블 채우기
+for i in range(1, len(str1)+1):
+    for j in range(1, len(str2)+1):
+        if str1[i-1] == str2[j-1]:
+            # 같으면: 둘 다 전진 +1
+            dp[i][j] = dp[i-1][j-1] + 1
+        else:
+            # 다르면: 둘 중 하나만 전진, 최대값
+            dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+
+
+
+print(dp[len(str1)][len(str2)])
+
+
+"""
+ACAYKP
+CAPCAK
+
+4
+"""
+
+# 못 풀었다. 탬플릿이니까 외우자.   
